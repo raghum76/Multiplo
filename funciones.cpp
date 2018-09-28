5b15f8e56fb3adbd97deaaeae8f75edffff251ef
glorianayeli <42391634+glorianayeli@users.noreply.github.com>
2018-09-27 19:41:25 -0500

    Update funciones.cpp

--- funciones.cpp
+++ funciones.cpp
@@ -2,14 +2,21 @@
 #include <iostream>
 #include <math.h>
 #include <cmath>
+#include <stdlib.h>
+#include <cstdlib>
 using namespace std;
-int formula2(int x,int y,float &funcion);
-int formula1(int x,float &funcion);
-int formula3(int x, float &funcion);
+float formula2(int x, int y);
+float formula1(float x);
+float formula3(float x);
+void tabular(int maximo);
+void tabular2(int maximo);
+void tabular3();
+
 int main()
 {
-    int op,maximo,x,res;
+    int op,maximo;
     float funcion;
+    cout<<"1)Formula2"<<endl<<"2)Formula1"<<endl<<"3)Formula3"<<endl;
     cout<<"Ingrese la funcion";
     cin>>op;
         while(op!=0)
@@ -18,33 +25,20 @@ int main()
             switch(op){
                 
                 case 1:
-                    cout<<"X"<<"\t"<<"Y"<<"\t"<<"f";
-                    for(x=1;x<=5;x++)
-                    {
-                        res=formula2(x,x,funcion);
-                        cout<<endl<<x<<"\t"<<x<<"\t"<<res;
-                    }
-                    
+                    system("cls");
+                    tabular3();
                     break;
                 case 2:
+                    system("cls");
                     cout<<"Ingrese el valor maximo";
                     cin>>maximo;
-                    cout<<"X"<<"\t"<<"f";//crear una funcion para tabular
-                    for(x=0;x<maximo;x++)
-                    {
-                        res=formula1(x,funcion);
-                        cout<<endl<<x<<"\t"<<res;
-                    }
+                    tabular(maximo);
                 break;
                 case 3:
+                    system("cls");
                     cout<<"Ingresa el valor maximo";
                     cin>>maximo;
-                    cout<<"X"<<"\t"<<"f";
-                    for(x=0;x<maximo;x++)
-                    {
-                        res=formula3(x,funcion);
-                        cout<<endl<<x<<"\t"<<res;
-                    }
+                    tabular2(maximo);
                 break;
             }
             cout<<endl<<"Ingrese la funcion";
@@ -55,27 +49,52 @@ int main()
     return 0;
 }
 
-int formula2(int x, int y,float &funcion){
+void tabular(int maximo){
+        cout<<"X"<<"\t"<<"f";//crear una funcion para tabular
+        for(int x=0;x<maximo;x++)
+            {
+                cout<<endl<<x<<"\t"<<formula1(x);
+            }
+}
+
+void tabular2(int maximo){
+    cout<<"X"<<"\t"<<"f";//crear una funcion para tabular
+    for(int x=0;x<maximo;x++)
+        {
+            cout<<endl<<x<<"\t"<<formula3(x);
+        }
+}
+
+void tabular3(){
+    cout<<"X"<<"\t"<<"Y"<<"\t"<<"f";
+    for(int x=1;x<=5;x++){
+        for(int y=1;y<=5;y++){
+            cout<<endl<<x<<"\t"<<y<<"\t"<<formula2(x,y);
+        }
+    }
+}
+
+float formula2(int x, int y){
     //Tabulare el valor en x y y de 0 a valor maximo
-    funcion=2*x*y-3*y;
-    return funcion;
+    x=2*x*y-(3*y);
+    return x;
     
 }
 
-int formula1(int x,float &funcion){
+float formula1(float x){
     double raiz=cbrt(2.0*pow(x,2.0));
-    funcion=raiz/(2.0*x-3.0);
-    return funcion;
+    x=raiz/(2.0*x-3.0);
+    return x;
 }
 
-int formula3(int x,float &funcion){
+float formula3(float x){
     if(x>0)
     {
-        funcion=2*(pow(x,3));
+        x=2*(pow(x,3));
     }
     else
     {
-        funcion=3*(pow(x,2));
+        x=3*(pow(x,2));
     }
-    return funcion;
+    return x;
 }
