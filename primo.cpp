a67e2bd7db6f422c3289d0188ad68acfddcb467b
glorianayeli <42391634+glorianayeli@users.noreply.github.com>
2018-10-19 22:03:06 -0500

    Create primo.cpp

---
+++ primo.cpp
@@ -0,0 +1,28 @@
+*******************************************************************************/
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+void imprimir(int v[]);
+
+int main()
+{
+    int v[999];
+    for(int i=0;i<1000;i++)
+        v[i]=1;
+    v[2]=0;
+    //Buscar subindices primos
+    for(int i=2;i<1000;i++)
+        for(int x=i+1;x<1000;x++)
+            if (x%i==0)
+                v[x]=0;
+    //Imprimir el vector
+    imprimir(v);
+
+    return 0;
+}
+void imprimir(int v[])
+{
+    for(int i=0;i<1000;i++)
+        cout<<"vec"<<"["<<i<<"]"<<"="<<v[i]<<endl;
+    
+}
